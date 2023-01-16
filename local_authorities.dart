a09cabd73a8467893d9422ef3509e76e16ebaa9f
georgefotop <119517831+georgefotop@users.noreply.github.com>
2023-01-16 02:46:31 +0200

    Add files via upload

---
+++ local_authorities.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_lorem/flutter_lorem.dart';
+
+void main() {
+  runApp(const LocalAuthorities());
+}
+
+class LocalAuthorities extends StatelessWidget {
+  const LocalAuthorities({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Local Authorities Articles',
+      home: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: const Text('Local Authorities',
+                style: TextStyle(
+                    color: Colors.lightBlue, fontWeight: FontWeight.bold)),
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+          ),
+          body: Center(
+            child: RandomWords(),
+          )),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({super.key});
+
+  @override
+  State<StatefulWidget> createState() => RandomWordState();
+}
+
+class RandomWordState extends State<RandomWords> {
+  final _suggestion = <String>[];
+  int _articleNumber = 1;
+  final _biggerFont = const TextStyle(fontSize: 18);
+  @override
+  Widget build(BuildContext context) {
+    final String = lorem(paragraphs: 4, words: 200);
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: (context, index) {
+          if (index.isOdd) return Divider();
+
+          final i = index ~/ 2;
+
+          if (i >= _suggestion.length) {
+            _suggestion.add(
+                "Article $_articleNumber: ${lorem(paragraphs: 2, words: 200)}");
+            _articleNumber++;
+            ;
+          }
+
+          return ListTile(
+            title: Text(
+              _suggestion[i],
+              style: _biggerFont,
+            ),
+          );
+        });
+  }
+}
